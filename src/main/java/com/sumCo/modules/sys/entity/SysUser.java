a5fa4ee5760abc171c38513332b963b2979f365d
alex <alex918855@gmail.com>
2023-08-17 14:17:11 +0800

    alex

--- src/main/java/com/sumCo/modules/sys/entity/SysUser.java
+++ src/main/java/com/sumCo/modules/sys/entity/SysUser.java
@@ -1,6 +1,6 @@
 package com.sumCo.modules.sys.entity;
-
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.hibernate.validator.constraints.NotBlank;
 
 import com.sumCo.common.validator.group.AddGroup;
@@ -17,7 +17,7 @@ import java.util.List;
  */
 @Data
 public class SysUser implements Serializable {
-	
+
 	/**
 	 * 用戶ID
 	 */
@@ -42,12 +42,12 @@ public class SysUser implements Serializable {
 	private String salt;
 
 
-	@NotBlank(message="郵箱不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="郵箱不能為空", groups = {AddGroup.class, UpdateGroup.class})
 //	@Email(message="郵箱格式不正確", groups = {AddGroup.class, UpdateGroup.class})
 	private String email;
 
 
-	@NotBlank(message="手機號碼不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="手機號碼不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String mobile;
 
 
@@ -55,7 +55,7 @@ public class SysUser implements Serializable {
 
 
 	private Integer status;
-	
+
 
 	private List<Long> roleIdList;
 
@@ -64,48 +64,50 @@ public class SysUser implements Serializable {
 
 	private Date createTime;
 	private String empAccount;
-//	@NotBlank(message="英文名稱不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="英文名稱不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String enName;
-//	@NotBlank(message="身分證字號不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="身分證字號不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String personId;
-//	@NotBlank(message="性別不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="性別不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String gender;
-//	@NotBlank(message="生日不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="生日不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
 	private Date birth;
-//	@NotBlank(message="年齡不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="年齡不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String age;
-//	@NotBlank(message="血型不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="血型不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String bloodType;
-//	@NotBlank(message="身分類別不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="身分類別不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String personType;
-//	@NotBlank(message="兵役狀況不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="兵役狀況不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String soldierType;
-//	@NotBlank(message="婚姻狀態不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="婚姻狀態不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String marriedType;
-//	@NotBlank(message="戶籍地址不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="戶籍地址不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String homeAddr;
-//	@NotBlank(message="聯絡地址不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="聯絡地址不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String contactAddr;
-//	@NotBlank(message="住家電話不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="住家電話不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String homePhone;
-//	@NotBlank(message="緊急聯絡人不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="緊急聯絡人不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String pressingPerson;
-//	@NotBlank(message="緊急連絡人關係不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="緊急連絡人關係不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String pressingRelation;
-//	@NotBlank(message="緊急連絡電話不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="緊急連絡電話不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String pressingPhone;
-//	@NotBlank(message="報到日期不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="報到日期不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
 	private Date registerDate;
-
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
 	private Date resignDate;
-//	@NotBlank(message="大頭貼不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="大頭貼不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private byte[] empPhoto;
 	private byte[] resignPhoto;
-//	@NotBlank(message="權限不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="權限不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String role;
-//	@NotBlank(message="剩餘特休時數不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="剩餘特休時數不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String yearHoliday;
-//	@NotBlank(message="底薪不能為空", groups = {AddGroup.class, UpdateGroup.class})
+	//	@NotBlank(message="底薪不能為空", groups = {AddGroup.class, UpdateGroup.class})
 	private String basicSalary;
 
 
@@ -124,7 +126,7 @@ public class SysUser implements Serializable {
 	public Long getId() {
 		return id;
 	}
-	
+
 	/**
 	 * 設置：用戶名
 	 * @param username 用戶名
@@ -164,7 +166,7 @@ public class SysUser implements Serializable {
 	public String getPassword() {
 		return password;
 	}
-	
+
 	/**
 	 * 設置：郵箱
 	 * @param email 郵箱
@@ -180,7 +182,7 @@ public class SysUser implements Serializable {
 	public String getEmail() {
 		return email;
 	}
-	
+
 	/**
 	 * 設置：手機號碼
 	 * @param mobile 手機號碼
@@ -196,7 +198,7 @@ public class SysUser implements Serializable {
 	public String getMobile() {
 		return mobile;
 	}
-	
+
 	/**
 	 * 設置：狀態  0：禁用   1：正常
 	 * @param status 狀態  0：禁用   1：正常
@@ -212,7 +214,7 @@ public class SysUser implements Serializable {
 	public Integer getStatus() {
 		return status;
 	}
-	
+
 	/**
 	 * 設置：創建時間
 	 * @param createTime 創建時間
@@ -421,6 +423,7 @@ public class SysUser implements Serializable {
 		this.resignPhoto = resignPhoto;
 	}
 
+	
 	public String getYearHoliday() {
 		return yearHoliday;
 	}
